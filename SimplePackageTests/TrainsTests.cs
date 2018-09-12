dbb5b8b6402e947a0d02c0374a4333d9c12fece7
Chris Mullins <cleemullins@users.noreply.github.com>
2018-09-12 15:03:53 -0700

    Added XUnit test project

---
+++ SimplePackageTests/TrainsTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SimplePackage;
+using Xunit;
+
+namespace SimplePackageTests
+{
+    public class TrainsTests
+    {
+        [Fact]
+        public void Trains_Should_Be_Awesome()
+        {
+            var t = new Trains();
+            Assert.True(t.AreTrainsAwesome);
+        }
+    }
+}
